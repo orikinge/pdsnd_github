610070d724286dd0ad68146b0a3fda5e78f8c24d
orikinge <71196951+orikinge@users.noreply.github.com>
2023-11-23 21:53:51 +0000

    fix: Call the user_details() function to enable user input when the program is run

--- bikeshare.py
+++ bikeshare.py
@@ -244,6 +244,7 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
+        user_details()
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
